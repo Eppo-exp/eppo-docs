243d52150c4d6e85f8570e0eca625f5cec069914
Blake Thomas <blake@geteppo.com>
2023-06-01 15:30:27 -0400

    dynamic import in web environment only

--- src/rudderstack.config.js
+++ src/rudderstack.config.js
@@ -1,12 +1,15 @@
-import * as rudderanalytics from 'rudder-sdk-js'
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
 
 let rudderstackReady = false
+let rudder
 
 if (ExecutionEnvironment.canUseDOM) {
-  rudderanalytics.load('2QZQtWRgHfXP5H2cnzSR8xE9ZBZ', 'https://geteppojohdkq.dataplane.rudderstack.com')
-  rudderanalytics.ready(() => {
-    rudderstackReady = true
+  import('rudder-sdk-js').then((rudderanalytics) => {
+    rudderanalytics.load('2QZQtWRgHfXP5H2cnzSR8xE9ZBZ', 'https://geteppojohdkq.dataplane.rudderstack.com')
+    rudderanalytics.ready(() => {
+      rudder = rudderanalytics
+      rudderstackReady = true
+    })
   })
 }
 
@@ -14,6 +17,6 @@ export async function onRouteDidUpdate ({ location, previousLocation }) {
   // Don't execute if we are still on the same page; the lifecycle may be fired
   // because the hash changes (e.g. when navigating between headings)
   if (ExecutionEnvironment.canUseDOM && rudderstackReady && location.pathname !== previousLocation?.pathname) {
-    rudderanalytics.page()
+    rudder.page()
   }
 }
