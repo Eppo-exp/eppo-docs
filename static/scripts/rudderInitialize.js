072fe79258525ab6e97da8af1edb3d45a078802c
Sven Schmit <sven@geteppo.com>
2023-06-01 10:44:02 -0700

    wip for Blake

--- static/scripts/rudderInitialize.js
+++ static/scripts/rudderInitialize.js
@@ -1,7 +1,11 @@
-export async function rudderInitialize() {
-    window.rudderanalytics = await import("rudder-sdk-js");
-    rudderanalytics.load("2QZQtWRgHfXP5H2cnzSR8xE9ZBZ", "https://geteppojohdkq.dataplane.rudderstack.com", {
-      integrations: { All: true }, // load call options
-    });
-  }
-  
+import * as rudderanalytics from "rudder-sdk-js";
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+
+if (ExecutionEnvironment.canUseDOM) {
+  rudderanalytics.load('2QZQtWRgHfXP5H2cnzSR8xE9ZBZ', 'https://geteppojohdkq.dataplane.rudderstack.com');
+  rudderanalytics.ready(() => {
+    console.log("We are all set!!!");
+  });
+}
+
+export { rudderanalytics };
--- static/scripts/rudderTrack.js
+++ static/scripts/rudderTrack.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { rudderInitialize } from "./rudderInitialize";
+import { rudderanalytics } from "./rudderInitialize";
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 export async function onRouteDidUpdate({location, previousLocation}) {
   // Don't execute if we are still on the same page; the lifecycle may be fired
   // because the hash changes (e.g. when navigating between headings)
-  if (location.pathname !== previousLocation?.pathname) {
-    await rudderInitialize()
-    window.rudderanalytics.page();
-    window.rudderanalytics.track('Sample Track Event');
+  if (ExecutionEnvironment.canUseDOM && location.pathname !== previousLocation?.pathname) {
+    // await rudderInitialize()
+    rudderanalytics.page();
+    rudderanalytics.track('Track Page Viewed');
   }
 }
