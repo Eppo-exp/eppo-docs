5ae8c4ad760d3d09812bb737d65e25cbfcebe019
lukasgoetzweiss <lukas@geteppo.com>
2024-06-10 11:03:16 -0600

    revert change to docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,7 @@ async function createConfig () {
 
     stylesheets: [
       {
-        href: '/css/custom.css',
+        href: 'https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css',
         type: 'text/css',
         integrity:
         'sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC',
