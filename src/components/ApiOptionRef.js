3391816a959c8f92904aaf53291bfa71be8c2627
Niko <39569270+nikomancy@users.noreply.github.com>
2024-11-25 09:56:24 -0700

    Adding optional type and type link feature. Making default optional.

--- src/components/ApiOptionRef.js
+++ src/components/ApiOptionRef.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import MDXContent from '@theme/MDXContent';
 
+<<<<<<< Updated upstream
 function ApiOptionRef({ name, children, defaultValue }) {
+=======
+function ApiOptionRef({ name, children, defaultValue, type, typeUrl }) {
+>>>>>>> Stashed changes
   return (
     <div className="api-option">
       <div className="api-option__header">
         <code className="api-option__name">{name}</code>
+<<<<<<< Updated upstream
+=======
+        
+        {type && (
+          <span className="api-option__type">
+            {typeUrl ? (
+              <a href={typeUrl} target="_blank" rel="noopener noreferrer">
+                {type}
+              </a>
+            ) : (
+              type
+            )}
+          </span>
+        )}
+
+>>>>>>> Stashed changes
         {defaultValue && (
           <span className="api-option__default">
             Default: <code>{defaultValue}</code>
