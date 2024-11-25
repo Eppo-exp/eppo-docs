01211e9da129ae7616533a4ebb1ee8538974869e
Niko <39569270+nikomancy@users.noreply.github.com>
2024-11-25 10:39:54 -0700

    Removing issue merge conflict artifact.

--- src/components/ApiOptionRef.js
+++ src/components/ApiOptionRef.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import MDXContent from '@theme/MDXContent';
 
-<<<<<<< Updated upstream
-function ApiOptionRef({ name, children, defaultValue }) {
-=======
 function ApiOptionRef({ name, children, defaultValue, type, typeUrl }) {
->>>>>>> Stashed changes
   return (
     <div className="api-option">
       <div className="api-option__header">
         <code className="api-option__name">{name}</code>
-<<<<<<< Updated upstream
-=======
         
         {type && (
           <span className="api-option__type">
@@ -25,7 +19,6 @@ function ApiOptionRef({ name, children, defaultValue, type, typeUrl }) {
           </span>
         )}
 
->>>>>>> Stashed changes
         {defaultValue && (
           <span className="api-option__default">
             Default: <code>{defaultValue}</code>
